d93c6475b89ece306390299cdac3a70777cfbd2b
Lyam <jwilli0508@gmail.com>
2019-06-21 10:50:11 -0500

    [Update] keys

--- config/key.js
+++ config/key.js
@@ -2,7 +2,7 @@ if(process.env.ENV==='prod'){
     module.exports = {
         googleClientID: process.env.GOOGLE_CLIENT,
         googleClientSecret: process.env.GOOGLE_SECRET,
-        mongoURL: process.env.MONGO_URL_PROD,
+        mongoURL: process.env.MONGO_URL,
         cookieKey: process.env.COOKIE_KEY
     };
 }else{
